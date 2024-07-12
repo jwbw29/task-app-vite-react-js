9ceab5c1b706db71f95c09dee4980aef33163b65
Justin Byrd <justinbyrd7@gmail.com>
2024-07-12 15:10:30 -0500

    Here: user created and signed in, fetching for user but fetch is failing

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,8 +1,8 @@
 // src/lib/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.SUPABASE_URL;
-const supabaseKey = import.meta.SUPABASE_ANON_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL and Key are required.");
 }

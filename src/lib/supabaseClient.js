e51a7fce2464b0d6212e07576eb108b57706ad2e
Justin Byrd <justinbyrd7@gmail.com>
2024-07-12 09:30:48 -0500

    added supabaseClient file

---
+++ src/lib/supabaseClient.js
@@ -0,0 +1,9 @@
+// src/lib/supabaseClient.js
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key are required.");
+}
+export const supabase = createClient(supabaseUrl, supabaseKey);

a07bf57e0970bd03749fe6529ccbf8f61ef7dbcb
Justin Byrd <justinbyrd7@gmail.com>
2024-07-12 10:21:47 -0500

    chore: Update supabaseClient.js and TasksPage.jsx to use import.meta.env for environment variables and fetch tasks based on user ID

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,9 +1,17 @@
 // src/lib/supabaseClient.js
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.SUPABASE_URL;
-const supabaseKey = import.meta.env.SUPABASE_ANON_KEY;
+const isDev = import.meta.env.DEV;
+
+const supabaseUrl = isDev
+  ? import.meta.env.VITE_SUPABASE_URL
+  : process.env.SUPABASE_URL;
+const supabaseKey = isDev
+  ? import.meta.env.VITE_SUPABASE_ANON_KEY
+  : process.env.SUPABASE_ANON_KEY;
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL and Key are required.");
 }
+
 export const supabase = createClient(supabaseUrl, supabaseKey);

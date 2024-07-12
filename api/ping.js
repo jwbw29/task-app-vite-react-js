5b5b9f5a400fa948ccac2bebfb7771a1dc0fa165
Justin Byrd <justinbyrd7@gmail.com>
2024-07-11 21:45:27 -0500

    chore: Add CORS headers to ping.js API endpoint

--- api/ping.js
+++ api/ping.js
@@ -1,12 +1,20 @@
-// api/ping.js
+// api/ping.js -- a simple query to ping the supabase database so that it doesn't turn off if there's no activity for 7 days
 import { createClient } from "@supabase/supabase-js";
 
-// Supabase client
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
+if (!supabaseUrl || !supabaseKey) {
+  throw new Error("Supabase URL and Key are required.");
+}
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default async (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   if (req.method === "GET") {
     // Simple query to keep the database awake
     const { data, error } = await supabase.from("tasks").select("id").limit(1);
